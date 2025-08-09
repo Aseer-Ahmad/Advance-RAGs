d795f0956df6d629fec5e7ea3164ce7dfdcbc018
Aseer-Ahmad <a3ahmad.kiit@gmail.com>
2025-08-09 16:36:31 +0200

    clip index upd

--- VLM/clip_index.py
+++ VLM/clip_index.py
@@ -36,7 +36,7 @@ class CLIP_RAG:
         self.embs = []
         self.metadatas = []
         batch_size = 2
-
+        print(f"creating embeddings")
         for i in range(0, len(self.dataset), batch_size):
             batch = self.dataset[i:i+batch_size]
             images = [Image.open(d["image_path"]).convert("RGB") for d in batch]
@@ -52,10 +52,13 @@ class CLIP_RAG:
                 image_emb = F.normalize(image_emb, dim=-1)
                 text_emb = F.normalize(text_emb, dim=-1)
                 
+                print(f"embeddings generated for {i}")
                 if self.fuse_type == "hadamard" : 
                     joint = self.fuse_hadamard(image_emb, text_emb)  # (B, D)
                 else : 
                     joint = self.fuse_add(image_emb, text_emb)  # (B, D)
+
+                print(f"embeddings fused for {i}")
 
             self.embs.append(joint.cpu().numpy().astype("float32"))
             for d in batch:
@@ -64,14 +67,18 @@ class CLIP_RAG:
                     "metadata_text": d["metadata_text"],
                 })
 
-        self.emb_matrix = np.vstack(self.embs)  # (N, D)            
+        self.emb_matrix = np.vstack(self.embs)  # (N, D)
+        self.createIndex()
+        self.save_Index_Metadata()      
 
     def createIndex(self):
+        print("creating index")
         D = self.emb_matrix.shape[1]
         self.index = faiss.IndexFlatIP(D)   
         self.index.add(self.emb_matrix)          
 
     def save_Index_Metadata(self):
+        print("saving index and metadata")
         faiss.write_index(self.index, "joint_index.faiss")
         pd.DataFrame(self.metadatas).to_parquet("metadatas.parquet")
 
@@ -80,6 +87,7 @@ class CLIP_RAG:
         self.metadf = pd.read_parquet(mtd_pth)
 
     def embed_query(self, query_str, query_img):
+        print("embedding query")
         inputs = self.processor(text=[query_str], images=[query_img], return_tensors="pt", padding=True).to(self.device)
 
         with torch.no_grad():
@@ -93,13 +101,15 @@ class CLIP_RAG:
 
         return joint.cpu().numpy().astype("float32")
     
-    def retreive(self, query_text, img_pth) : 
-        query_image = Image.open(img_pth).convert("RGB")
-        q_emb = embed_query(query_text, query_image)
+    def retreive(self, query_text, img_pth, k) : 
 
-        k = 5
-        scores, indices = index.search(q_emb, k)  # scores are inner products
-        results = metadf.iloc[indices[0]].to_dict(orient="records")
+        print("retreiving")
+        query_image = Image.open(img_pth).convert("RGB")
+        q_emb = self.embed_query(query_text, query_image)
+
+        scores, indices = self.index.search(q_emb, k)  # scores are inner products
+        results = self.metadatas[indices[0]]
+        # results = self.metadf.iloc[indices[0]].to_dict(orient="records")
 
         print("Top results:", results)
         print("Scores:", scores[0])
@@ -108,9 +118,12 @@ class CLIP_RAG:
 if __name__ == '__main__' : 
     
     dataset = [
-        {"image_path": "/data/imgs/apple.jpg", "metadata_text": "apple"},
-        {"image_path": "/data/imgs/clock.jpg", "metadata_text": "clock"},
+        {"image_path": "data/imgs/apple.jpg", "metadata_text": "apple"},
+        {"image_path": "data/imgs/clock.jpg", "metadata_text": "clock"},
     ]
 
-    # clipindex = CLIP_index(dataset)
-    # clipindex.setModel()
+    clipindex = CLIP_RAG(dataset, fuse_type="hadamard")
+    clipindex.setModel()
+    clipindex.generateEmbeddings()
+
+    clipindex.retreive("what is this", "data/imgs/apple.jpg", k = 1)
